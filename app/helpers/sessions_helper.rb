89fe56ff7279d8ace66a1903e3917bf2237bcbcc
micchan <p1r2yy9knr@gmail.com>
2020-01-17 15:52:01 +0900

    投稿、投稿編集、投稿削除機能を追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,11 @@ module SessionsHelper
   def logged_in?
     current_user.present?
   end
+
+  def authenticate_user
+    if current_user.nil?
+      redirect_to new_session_url, notice: "ログインが必要です"
+    end
+  end
+
 end
