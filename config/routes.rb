64d2c3437a5673f7e7ede2039adaf4885ebf743c
micchan <p1r2yy9knr@gmail.com>
2020-01-17 16:36:36 +0900

    お気に入り機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :pictures
+  resources :favorites, only: %i(create destroy)
   resources :sessions, only: %i(new create destroy)
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :pictures
 end
