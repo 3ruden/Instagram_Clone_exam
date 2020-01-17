e5fa4d6da52b7130ed214c846021a3771064102c
micchan <p1r2yy9knr@gmail.com>
2020-01-18 01:53:01 +0900

    お気にした投稿の一覧を表示する機能を追加、デザインを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   include UsersHelper
   before_action :authenticate_user, only: %i(index show edit update destroy)
   before_action :forbid_login_user, only: %i(new create)
-  before_action :set_user, only: %i(show edit update destroy)
+  before_action :set_user, only: %i(show edit update destroy likes)
   before_action :ensure_correct_user, only: %i(edit update destroy)
 
   def index
@@ -55,12 +55,15 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def likes
+  end
 
-    def user_params
-      params.require(:user).permit(:profile_image, :profile_image_chache, :name, :email, :password, :password_confirmation)
-    end
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:profile_image, :profile_image_chache, :name, :email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root 'home#top'
   resources :favorites, only: %i(create destroy)
   resources :sessions, only: %i(new create destroy)
-  resources :users
+  resources :users do
+    member do
+      get :likes
+    end
+  end
   resources :pictures
   mount LetterOpenerWeb::Engine, at: "/inbox" if Rails.env.development?
 end
